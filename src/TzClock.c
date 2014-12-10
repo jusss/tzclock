cb75321275421bd6e6b706c74a0e1d509c627a3d
Chris Knight <chris@theknight.co.uk>
2014-12-10 08:00:26 +0000

    Use tzset instead of calling local time twice

--- src/TzClock.c
+++ src/TzClock.c
@@ -2460,7 +2460,7 @@ void getTheFaceTime (FACE_SETTINGS *faceSetting, time_t *t, struct tm *tm)
 	{
 		setenv ("TZ", timeZones[timeZone].envName, 1);
 	} 
-	localtime (t);
+	tzset();
 	localtime_r (t, tm);
 }			
 
